ce5bdda6f1296240c7e469bc26f5022ccde9c1b1
ava brenneke <avabrenneke@gmail.com>
2021-01-16 19:01:41 -0800

    added initial constants to app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+// Dependencies 
+var inquirer = require("inquirer");
+var connection = require('./connection');
+
+//Table Choices
+const tableOptions = [
+    "Departments",
+    "Roles",
+    "Employees",
+    "Update Employee",
+    "exit"
+];
+//Employee List
+const employeeOptions = [
+    "Ava Brenneke",
+    "Omar Teach",
+    "Matt Subteach",
+    "Peter Parker",
+    "Chris Brown",
+    "Post Malone",
+    "exit"
+];
+//Table Options
+const tableOptions = [
+    "First Name",
+    "Last Name",
+    "Role",
+    "exit"
+];
